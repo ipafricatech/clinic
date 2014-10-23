00b87a92c95076dfae65ae2a4b835deb3657b5c5
sifisob <stillpledged@gmail.com>
2014-10-23 15:13:05 +0200

    Imitial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clinic::Application.config.secret_token = '077ebc7dacb8c470a6eccbffff73e640a9f1072b22056ffd02e604c070406c977292599d6dba8acad74cd674670f607c4ad1acc2954d469453f3010a4207f257'
